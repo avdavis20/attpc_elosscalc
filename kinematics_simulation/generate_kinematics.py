e2df1e9308f528d0d8a886476526b5519c2dec68
avdavis20 <124411444+avdavis20@users.noreply.github.com>
2024-07-11 17:48:05 +0000

    update-not fixed

--- kinematics_simulation/generate_kinematics.py
+++ kinematics_simulation/generate_kinematics.py
@@ -1,4 +1,3 @@
-# IMPORT LIBRARIES
 from attpc_engine.kinematics import (
     KinematicsPipeline,
     KinematicsTargetMaterial,
@@ -11,49 +10,34 @@ from attpc_engine import nuclear_map
 from spyral_utils.nuclear.target import TargetData, GasTarget
 from pathlib import Path
 import numpy as np
-import json
 
+output_path = Path("./output/kinematics/kinsimtest.h5")
 
-# INITIALIZE OUTPUT PATH
-output_path = Path('/workspaces/attpc_elosscalc/kinematics_simulation/output/kinematics/kinsimtest.h5')
-
-
-# OPEN JSON CONFIG FILE
-with open('/workspaces/attpc_elosscalc/eloss_program/eloss_config.json') as f:
-  data = json.load(f)
-
-
-# DEFINE NUM OF EVENTS AND BEAM ENERGY
-nevents = 10000
-beam_energy = 184.131 # MeV
-
-
-# DEFINE TARGET
 target = GasTarget(
-  TargetData(compound = [(1,2,2)],pressure = 300.0, thickness = None),
-  nuclear_map
-  )
-
-
-# DEFINE REACTION CONSITITUENTS
-pipeline = KinematicsPipeline(
-  [
-    Reaction(
-    target = nuclear_map.get_data(1,2),
-    projectile = nuclear_map.get_data(6,16),
-    ejectile = nuclear_map.get_data(1,2), 
-    )
-  ],
-  [PolarUniform(0.0, np.pi)],
-  [ExcitationGaussian(0.0,0.001)],
-  beam_energy=184.131,
-  target_material=KinematicsTargetMaterial(
-  material = target, z_range = (0.0,1.0), rho_sigma = 0.007
-  ),
+    TargetData(compound=[(1, 2, 2)], pressure=300.0, thickness=None), nuclear_map
 )
 
+nevents = 10000
 
-# EXECUTE TASKS
+beam_energy = 184.131 # MeV
+
+pipeline = KinematicsPipeline(
+    [
+        Reaction(
+            target=nuclear_map.get_data(1, 2), # deuteron
+            projectile=nuclear_map.get_data(6, 16), # 16C
+            ejectile=nuclear_map.get_data(1, 2), # deuteron
+        )
+    ],
+    [ExcitationGaussian(0.0, 0.001)], # No width to ground state
+    [PolarUniform(0.0, np.pi)], # Full angular range 0 deg to 180 deg
+    beam_energy=184.131, # MeV
+    target_material=KinematicsTargetMaterial(
+        material=target, z_range=(0.0, 1.0), rho_sigma=0.007
+    ),
+)
 def main():
-  run_kinematics_pipeline(pipeline, nevents, output_path)
+    run_kinematics_pipeline(pipeline, nevents, output_path)
 
+if __name__ == "__main__":
+    main()
